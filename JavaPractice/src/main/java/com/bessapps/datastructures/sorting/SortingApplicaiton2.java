c96047ae17be7490bdcc7e90760a4126ea88051d
ravi saladi <rvk40413@gmail.com>
2021-07-27 10:31:23 -0400

    new files added

---
+++ JavaPractice/src/main/java/com/bessapps/datastructures/sorting/SortingApplicaiton2.java
@@ -0,0 +1,31 @@
+package com.bessapps.datastructures.sorting;
+
+class SortClass {
+
+    public int[] bubbleSort(int[] arr) {
+
+    }
+
+    public int[] selectionSort(int[] arr) {
+
+    }
+
+    public int[] mergeSort(int[] arr) {
+
+    }
+
+    public int[] quickSort(int[] arr) {
+
+    }
+
+    public int[] inserstionSort(int[] arr) {
+
+    }
+
+}
+
+public class SortingApplicaiton2 {
+    public static void main(String args[]) {
+        SortClass sort = new SortClass();
+    }
+}
